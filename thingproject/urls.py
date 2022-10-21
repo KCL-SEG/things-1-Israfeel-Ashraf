cc2202e3b91716868fc9c6774339b58c296763a8
Israfeel Ashraf <israfeel.ashraf@kcl.ac.uk>
2022-10-21 12:39:37 +0100

    Add app and home view

--- thingproject/urls.py
+++ thingproject/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from things import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #Remember that '' is the home page.
+    path('', views.home, name='home')
 ]
